03cc6ced605d7dc60ed21773f42dd6024579a44a
Hansuk Hong <flavono123@gmail.com>
2018-01-20 22:27:37 +0900

    model: create a simple, Wanted style, resume.models.Resume
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# v 0.1.0 (Wanted style)
+class Resume(models.Model):
+    # essential fields
+    name = models.CharField(max_length=25)
+    base_info = models.TextField()
+    # optional fields
+    career = models.TextField(blank=True)
+    education = models.TextField(blank=True)
+    award = models.TextField(blank=True)
+    language = models.TextField(blank=True)
+    link = models.TextField(blank=True)
--- resume/tests.py
+++ resume/tests.py
@@ -4,6 +4,8 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from .views import index_page
+from .models import Resume
+
 
 class IndexViewTest(TestCase):
     
@@ -17,3 +19,23 @@ class IndexViewTest(TestCase):
         expected_html = render_to_string('resume/index.html')
 
         self.assertEqual(expected_html, response.content.decode())
+
+
+class ResumeModelTest(TestCase):
+
+    def test_saving_resumes(self):
+        first_resume = Resume(name='에잇퍼센트', base_info='홍한석\n010-6434-3473\nflavono123@gmail.com')
+        first_resume.save()
+        second_resume = Resume(name='아테나스랩', base_info='홍한석\n010-6434-3473\nflavono123@gmail.com')
+        second_resume.save()
+       
+        saved_items = Resume.objects.all()
+
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_resume = saved_items[0] 
+        second_saved_resume = saved_items[1]
+
+        self.assertEqual(first_saved_resume, first_resume)
+        self.assertEqual(second_saved_resume, second_resume)
+
