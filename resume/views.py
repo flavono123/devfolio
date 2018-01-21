311f6da66b94ad828883b136133c9eee551b015a
Hansuk Hong <flavono123@gmail.com>
2018-01-21 12:18:49 +0900

    template: render form table, submit by post msg
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- resume/views.py
+++ resume/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .forms import ResumeForm
+
 
 def index_page(request):
-    return render(request, 'resume/index.html')
+    if request.method == 'POST':
+        form = ResumeForm(request.POST, request.FILES)
+        if form.is_valid():
+            pass
+    else:
+        form = ResumeForm()
+    return render(request, 'resume/index.html', {
+            'form': form,
+        })
