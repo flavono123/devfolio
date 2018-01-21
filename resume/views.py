2fe57426e33217d66b2651e8e6998d888671d761
Hansuk Hong <flavono123@gmail.com>
2018-01-21 17:47:08 +0900

    render devicons tricky way
    - ref:
    https://stackoverflow.com/questions/31710147/d3-bootstrap-responsively-centering-svg
    
    (I think js framework for front-end web components..?)
    
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- resume/views.py
+++ resume/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
+
+import json
 
 from .forms import ResumeForm
 from .models import Resume
 
 
 def index_page(request):
-    resume_list = Resume.objects.all()
     if request.method == 'POST':
         form = ResumeForm(request.POST, request.FILES)
         if form.is_valid():
@@ -13,9 +15,16 @@ def index_page(request):
             return redirect('resume:list')
     else:
         form = ResumeForm()
+    
+    svg_json_path = settings.ROOT('devfolio', 'static', 'svg_codes.json')
+    with open(svg_json_path, 'r') as f:
+        svg_dict = json.load(f)
+    resume_list = Resume.objects.all()
+
     return render(request, 'resume/index.html', {
         'form': form,
         'resume_list': resume_list,
+        'svg_dict': svg_dict,
     })
 
 def list(request):
