35bdb88304f6e54cf21f6b2aae7745dec2d9ccfd
Hansuk Hong <flavono123@gmail.com>
2018-01-29 15:35:22 +0900

    redirect home to resume index
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- devfolio/urls.py
+++ devfolio/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda r: redirect('resume:index')),
+
     path('admin/', admin.site.urls),
     path('resume/', include('resume.urls')),
     path('accounts/', include('accounts.urls')),
-
 ]
--- resume/urls.py
+++ resume/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'resume'
 
 urlpatterns = [
-    path('', views.index_page, name='index_page'),
+    path('', views.index_page, name='index'),
     path('list/', views.list, name='list'),
 ]
