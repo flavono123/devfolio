7ef5ddeb6c50c58c6714e95e13ff31c52fb9757c
Hansuk Hong <flavono123@gmail.com>
2018-01-29 18:54:46 +0900

    simple login/out singin
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.login, name='login', kwargs={
+        'template_name': 'accounts/login.html',
+    }),
+    path('logout/', auth_views.logout, name='logout', kwargs={
+        'next_page': settings.LOGIN_URL,
+    }),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.conf import settings
 
+from .forms import SignupForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = SignupForm(request.POST)
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
 
 def profile(request):
     return render(request, 'accounts/profile.html')
