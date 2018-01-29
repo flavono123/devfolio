afb8e66427c531346fb0d5fc3f47a32a429c7fd8
Hansuk Hong <flavono123@gmail.com>
2018-01-30 01:36:31 +0900

    signup with email as username
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,22 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, get_user_model
 
 
 class SignupForm(UserCreationForm):
-    pass 
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
+        widgets = {
+            'username': forms.EmailInput(attrs={
+                'placeholder': 'Email',
+            }),
+            # TODO: placeholders for password and confirm
+            'email': forms.HiddenInput,     
+        }
+
+    def save(self, commit=True):
+        user = super().save(False)
+        user.email = user.username
+        user = super().save()
+
+        return user
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 
 from .forms import SignupForm
@@ -6,7 +6,7 @@ from .forms import SignupForm
 
 def signup(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
+        form = SignupForm(request.POST, initial={'username': 'temp'})
         if form.is_valid():
             form.save()
             return redirect(settings.LOGIN_URL)
