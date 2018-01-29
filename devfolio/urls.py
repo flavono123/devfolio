1bb3091a012784fd5e8e478a740f03a2201f2e75
Hansuk Hong <flavono123@gmail.com>
2018-01-29 15:32:39 +0900

    new app accounts for users
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- devfolio/urls.py
+++ devfolio/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('resume/', include('resume.urls')),
+    path('accounts/', include('accounts.urls')),
 
 ]
