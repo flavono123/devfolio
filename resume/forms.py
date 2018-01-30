5d410dd4665a001868c54f775e13c21daf85d384
Hansuk Hong <flavono123@gmail.com>
2018-01-30 16:39:05 +0900

    create resume.models.Career/.forms.CareerForm
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- resume/forms.py
+++ resume/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Resume
+from .models import Resume, Career
 
 
 class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
+        fields = '__all__'
+
+
+class CareerForm(forms.ModelForm):
+    class Meta:
+        model = Career
         fields = '__all__'
--- resume/models.py
+++ resume/models.py
@@ -7,13 +7,20 @@ class Resume(models.Model):
     name = models.CharField(max_length=25)
     base_info = models.TextField()
     # optional fields
-    career = models.TextField(blank=True)
     education = models.TextField(blank=True)
     award = models.TextField(blank=True)
-    language = models.TextField(blank=True)
     link = models.TextField(blank=True)
     # auto fields
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+class Career(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    company = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    since = models.DateField()
+    until = models.DateField(blank=True)
+    currently_employed = models.BooleanField()
+    
