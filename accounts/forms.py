dad41c05f731500b0ae2eb90367863e6a1cbdb9b
Hansuk Hong <flavono123@gmail.com>
2018-01-30 18:31:27 +0900

    custom login form for bootstrap login page
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,22 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 
 
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.EmailInput(attrs={
+        'id': 'inputEmail',
+        'class': 'form-control',
+        'placeholder': 'Email',
+        'autofocus': True,
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'id': 'inputPassword',
+        'class': 'form-control',
+        'placeholder': 'Password',
+    }))
+
+    
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         #TODO: make the first and the last name to essential fields / blank=False
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 from . import views
+from .forms import LoginForm
 
 
 urlpatterns = [
@@ -10,6 +11,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.login, name='login', kwargs={
         'template_name': 'accounts/login.html',
+        'authentication_form': LoginForm,
     }),
     path('logout/', auth_views.logout, name='logout', kwargs={
         'next_page': settings.LOGIN_URL,
