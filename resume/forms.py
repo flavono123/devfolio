4227a0683350e2e6bbadbbc34055a196da1441b1
Hansuk Hong <flavono123@gmail.com>
2018-01-31 21:45:17 +0900

    render Bootswatch cutom checkbox(hard-code)
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- resume/forms.py
+++ resume/forms.py
@@ -13,4 +13,10 @@ class CareerForm(forms.ModelForm):
     class Meta:
         model = Career
         fields = '__all__'
-
+        widgets = {
+            'company': forms.TextInput(attrs={'class': 'form-control'}),
+            'position': forms.TextInput(attrs={'class': 'form-control'}),
+            'since': forms.TextInput(attrs={'class': 'form-control'}),
+            'until': forms.TextInput(attrs={'class': 'form-control'}),
+            'currently_employed': forms.CheckboxInput(attrs={'class': 'custom-control-input'}),
+        }
