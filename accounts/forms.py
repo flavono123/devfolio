03fd4d46fa24062e65478c520bd5e2607edb6081
Hansuk Hong <flavono123@gmail.com>
2018-01-31 13:57:44 +0900

    accounts.models.Profile for user identicon
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 
+from .models import Profile
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.EmailInput(attrs={
