cdc0a6f00f828c799b81ee7a6c388fbdbb1879af
Hansuk Hong <flavono123@gmail.com>
2018-02-15 14:35:41 +0900

    remove unused urls and view funcs
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- resume/urls.py
+++ resume/urls.py
@@ -8,10 +8,4 @@ app_name = 'resume'
 urlpatterns = [
     path('list/', views.resume_list, name='list'),
     path('new', views.resume_form, name='form'),
-    
-    
-    path('career/new', views.career_form, name='career_form'),
-    path('education/new', views.education_form, name='education_form'),
-    path('award/new', views.award_form, name='award_form'),
-    path('link/new', views.link_form, name='link_form'),
 ]
